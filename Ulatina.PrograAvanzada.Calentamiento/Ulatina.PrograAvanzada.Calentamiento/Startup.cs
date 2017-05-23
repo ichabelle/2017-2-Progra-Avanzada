121ee3000e9ad683fa55352dfee60fdc8b8d05d7
Héctor Fernández <hecferme01.cr@hotmail.com>
2017-05-22 19:54:41 -0600

    Proyecto con aplicación web base

---
+++ Ulatina.PrograAvanzada.Calentamiento/Ulatina.PrograAvanzada.Calentamiento/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ulatina.PrograAvanzada.Calentamiento.Startup))]
+namespace Ulatina.PrograAvanzada.Calentamiento
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
